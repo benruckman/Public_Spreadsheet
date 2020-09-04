2e1533463f7c725e2884e641d99d540a6ffd9921
mikey <mikeyeyer098@gmail.com>
2020-09-03 21:40:11 -0700

    added handling for * and /, works no debugging

--- Spreadsheet/FormulaEvaluatorConsoleTester/Program.cs
+++ Spreadsheet/FormulaEvaluatorConsoleTester/Program.cs
@@ -16,6 +16,11 @@ namespace FormulaEvaluatorConsoleTester
             Console.WriteLine(FormulaEvaluator.Evaluator.Evaluate("2+1+2-3-3", variableEvaluator));
             Console.WriteLine(FormulaEvaluator.Evaluator.Evaluate("2+1+a4", variableEvaluator));
             Console.WriteLine(FormulaEvaluator.Evaluator.Evaluate("2+1+a4-bb67", variableEvaluator));
+            Console.WriteLine(FormulaEvaluator.Evaluator.Evaluate("2*2", variableEvaluator));
+            Console.WriteLine(FormulaEvaluator.Evaluator.Evaluate("2*2+1", variableEvaluator));
+            Console.WriteLine(FormulaEvaluator.Evaluator.Evaluate("1+2*2", variableEvaluator));
+            Console.WriteLine(FormulaEvaluator.Evaluator.Evaluate("2*2-1", variableEvaluator));
+            Console.WriteLine(FormulaEvaluator.Evaluator.Evaluate("1-2*2", variableEvaluator));
             Console.WriteLine("Hello World!");
             Console.ReadKey();
         }
